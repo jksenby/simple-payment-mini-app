27309a891d2995ab2f764a06d667bffa2d763eaa
jksenby <zhalgas.karsenbai@gmail.com>
2025-06-20 01:19:51 +0500

    added tailwind and template for payment, uninstalled ant design

--- src/app/components/payment/payment.component.ts
+++ src/app/components/payment/payment.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
-import { NzButtonModule } from 'ng-zorro-antd/button';
-import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-payment',
-  imports: [NzButtonModule, NzIconModule],
   templateUrl: './payment.component.html',
   styleUrl: './payment.component.scss'
 })
