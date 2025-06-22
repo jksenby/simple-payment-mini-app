e23cb52f0358ff5c3079b63de1dd76b6f4bc31dd
jksenby <zhalgas.karsenbai@gmail.com>
2025-06-23 01:13:35 +0500

    added email fiend and form module for payment(and launch button in tg)

--- backend/telegram-bot/main.py
+++ backend/telegram-bot/main.py
@@ -1,3 +1,5 @@
+from operator import truediv
+
 from telegram.ext import Application, MessageHandler, filters, CommandHandler
 from dotenv import load_dotenv
 import os
@@ -6,13 +8,23 @@ load_dotenv()
 
 async def reply(update, context):
   await update.message.reply_text("Hello there!")
+  print("1")
 
 async def reply_to_photo(update, context):
   await update.message.reply_text("No Vibe!")
 
+async def set_menu_button(application):
+  await application.bot.set_chat_menu_button(
+    menu_button={"type": "web_app", "text": "Launch", "web_app": {"url": "https://simple-payment-mini-app-54db.vercel.app"}}
+  )
+
+async def post_init(application):
+  """This runs after the application is initialized but before polling starts"""
+  await set_menu_button(application)
+
 def main():
     token = os.getenv("TELEGRAM_BOT_TOKEN")
-    application = Application.builder().token(token).concurrent_updates(True).read_timeout(30).write_timeout(30).build()
+    application = Application.builder().token(token).post_init(post_init).concurrent_updates(True).read_timeout(30).write_timeout(30).build()
     application.add_handler(CommandHandler("startapp", reply))
     application.add_handler(MessageHandler(filters.PHOTO, reply_to_photo))
     print("Telegram Bot started!", flush=True)
